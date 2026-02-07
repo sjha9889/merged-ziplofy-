0dc825f0fef4b31c79dcd65dea22092c49bd700d
developer200419-ship-it <developer200419@gmail.com>
2026-02-07 12:39:12 +0530

    fix: add allowed hosts in preview allowd hosts of frontend vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    allowedHosts:["https://admin.ziplofy.com"]
-  }
+  server: {
+    allowedHosts: ['admin.ziplofy.com'],
+  },
+  preview: {
+    allowedHosts: ['admin.ziplofy.com'],
+  },
 })
