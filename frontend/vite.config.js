9da9c337d347bbbbb6f2345a11a7b30f26fd8ca7
sjha9889 <118978364+sjha9889@users.noreply.github.com>
2026-02-09 12:08:22 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['admin.ziplofy.com'],
+  },
+  preview: {
+    allowedHosts: ['admin.ziplofy.com'],
+  },
 })
