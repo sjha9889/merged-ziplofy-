f8eceb1c52b6d832d0a475ec4bcfca498248a571
RishiBakshi <developer200419@gmail.com>
2026-04-28 15:43:35 +0530

    feat: add updated builds

---
+++ Ziplofy/dist/assets/ContentMetaObjectsPage-BMn2gBRd.js
@@ -0,0 +1,1 @@
+import t from"./ContentPage-VF7gyL2J.js";import"./index-Cq_rh5Mx.js";const a=t;export{a as ContentMetaObjectsPage};
---
+++ Ziplofy/dist/assets/SettingsIndex-D_iUR4yf.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-Cq_rh5Mx.js";import e from"./SettingsPlaceholder-B9PTLpRM.js";import"./SettingsPageScaffold-C93I4avB.js";const i=()=>t.jsx(e,{title:"General"});export{i as default};
