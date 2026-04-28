6468ddb7b6f0fc7e2eb946848ea45602387dca06
RishiBakshi <developer200419@gmail.com>
2026-04-28 17:03:05 +0530

    feat: server folder ci cd updated test

--- server/src/app.ts
+++ server/src/app.ts
@@ -17,7 +17,7 @@ app.use(cors({
 
 app.get('/api/health', (req: Request, res: Response) => {
   res.status(200).json({
-    status: 'ok -updated',
+    status: 'ok -updated ci cd updated',
     timestamp: new Date().toISOString(),
     uptime: process.uptime(),
   });
